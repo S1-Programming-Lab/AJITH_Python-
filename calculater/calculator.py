f5c23c61440ede90679bb4a7eacede06e180986c
AJITHPS7 <psajith987@gmail.com>
2022-03-02 15:29:46 +0530

    Python

---
+++ calculater/calculator.py
@@ -0,0 +1,10 @@
+def addition(x,y):
+        result=x+y
+        return result
+def substration(x,y):
+        return x-y
+def multiplication(x,y):
+        result=x*y
+        return result
+def division(x,y):
+        return float(x/y)
